ea9a9144cb7ffceb2f2673d143e67c65e3b62519
jjangjun0 <jangjun012@gmail.com>
2026-06-21 04:42:20 +0900

    init commit

---
+++ src/test/java/com/example/memo_practice/MemoPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memo_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
